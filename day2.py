cf6a7d1a08fdf3b280d4adb15f1cf84eacf4932a
Mark Cheli <mcheli@ptc.com>
2021-12-03 01:11:42 +0000

    Completed day 2 part 2!

--- day2.py
+++ day2.py
@@ -7,18 +7,20 @@ d_values = d.splitlines()
 
 depth = 0
 position = 0
+aim = 0
 
 for index, movement in enumerate(d_values):
   mov_array = movement.split( )
   if mov_array[0] == "forward":
-    print("moved forward")
+    print(f"moved forward by {mov_array[1]}")
     position += int(mov_array[1])
+    depth += aim * int(mov_array[1])
   elif mov_array[0] == "up":
-    print("moved up")
-    depth -= int(mov_array[1])
+    print(f"moved up by {mov_array[1]}")
+    aim -= int(mov_array[1])
   elif mov_array[0] == "down":
-    print("moved up")
-    depth += int(mov_array[1])
+    print(f"moved up by {mov_array[1]}")
+    aim += int(mov_array[1])
   else:
     print("No idea what happened here")
 
